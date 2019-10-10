76d17d781f5c681568049451dd2bab7fc8e116a4
Hanna463 <aniamarko21@gmail.com>
2019-10-10 14:30:41 +0000

    new

--- js_index.js
+++ js_index.js
@@ -1,92 +1,23 @@
-const link = document.querySelector('a');
-
-link.textContent = 'Programming Mentor';
-
-link.href = 'https://sj42.programmingmentor.com';
-
-
-
-const sect = document.querySelector('section');
-
-const para = document.createElement('p');
-
-para.textContent = 'We hope you enjoyed the ride.';
-
-sect.appendChild(para);
-
-
-
-const text = document.createTextNode(' — the premier source for web development knowledge.');
-
-const linkPara = document.querySelector('p');
-
-linkPara.appendChild(text);
-
-
-
-sect.appendChild(linkPara.cloneNode(true));
-
-
-
-sect.removeChild(document.querySelectorAll('p')[document.querySelectorAll('p').length-1]);
-
-
-
-para.style.color = 'white';
-
-para.style.backgroundColor = 'black';
-
-para.style.padding = '10px';
-
-para.style.width = '250px';
-
-para.style.textAlign = 'center';
-
-
-
-para.classList.add('selected');
-
-
-
-const button = document.createElement('button');
-
-button.innerText = 'Click Me!';
-
-sect.appendChild(button);
-
-button.addEventListener('click', () => alert('Hello!') );
-
-
-
-// Clock
-
-const clock = document.querySelector('.clock');
-
-setInterval( () => clock.innerText = new Date().toLocaleTimeString(),
-
-        1000 );
-
-
-
-// Calc
-
-const display = document.querySelector('.calc .display');
-
-document.querySelectorAll('.calc .buttons button')
-
-    .forEach( btn => btn.addEventListener('click', btnClicked));
-
-
-
-function btnClicked(ev) {
-
-    display.value += ev.target.innerText;
-
+function insert(num) {
+    document.form.textview.value += num;
 }
-
-
-
-document.querySelector('.calc .eq').addEventListener('click' ,
-
-  () => display.value = eval(display.value) );
-
+function clean() {
+     document.form.textview.value = "";
+}
+function back() {
+    let exp = document.form.textview.value;
+     document.form.textview.value = exp.substring(0,exp.length-1);
+}
+function equal() {
+   let exp = document.form.textview.value;
+   if(exp) {
+       document.form.textview.value = eval(exp);
+   }
+}
+ if (!isFinite(resultNum)) {
+      if (isNaN(resultNum)) {
+        resultNum = "You broke it!";
+      } else {
+        resultNum = "Look at what you've done";
+      }
+ }
