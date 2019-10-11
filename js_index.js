018578b620cb56537f1dd7d23174c78dce50b89f
Hanna463 <aniamarko21@gmail.com>
2019-10-11 08:10:28 +0000

    new name

--- js_index.js
+++ js_index.js
@@ -2,22 +2,15 @@ function insert(num) {
     document.form.textview.value += num;
 }
 function clean() {
-     document.form.textview.value = "";
+    document.form.textview.value = "";
 }
 function back() {
     let exp = document.form.textview.value;
-     document.form.textview.value = exp.substring(0,exp.length-1);
+        document.form.textview.value = exp.substring(0,exp.length-1);
 }
 function equal() {
    let exp = document.form.textview.value;
    if(exp) {
-       document.form.textview.value = eval(exp);
+        document.form.textview.value = eval(exp);
    }
 }
- if (!isFinite(resultNum)) {
-      if (isNaN(resultNum)) {
-        resultNum = "You broke it!";
-      } else {
-        resultNum = "Look at what you've done";
-      }
- }
